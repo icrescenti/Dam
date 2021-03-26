8c1b3784bb5238b80c291fef49ac224b17a91845
icrescenti_linux <46972850+icrescenti@users.noreply.github.com>
2021-03-26 17:53:47 +0100

    odoo customcrm functions started

--- dam_odoo/custom_crm/models/models.py
+++ dam_odoo/custom_crm/models/models.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
+from datetime import datetime
 
 class Visit(models.Model):
     _name = 'custom_crm.visit'
     _description = 'Visit'
 
     name = fields.Char(string='Descripció')
-    customer = fields.Char(string='Client')
-    date = fields.Datetime(string='Data')
+    customer = fields.Many2one(comodel_name='res.partner', string='Client')
+    date = fields.Integer(compute='_compute_difference')
     type = fields.Selection([('P','Presencial'),('W','Whatsapp'),('T','Telefon')], string='Tipus',required=True)
     done = fields.Boolean(string='Fet',readonly=True)
+
+def _compute_difference(self):
+    date = 20
+    #for rec in self:
+        #rec.days_difference = (datetime.date.time.today()- rec.created_date).days
