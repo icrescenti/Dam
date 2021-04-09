8023ebb78fd9a872654dccdabe44202e9f11a829
icrescenti_linux <46972850+icrescenti@users.noreply.github.com>
2021-04-09 17:28:03 +0200

    Proves Unitaries

---
+++ dam_wpf/UF2/EcuacionPrimerGrado/ProvesUnitaries/EcuacionPrimerGradoTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using EcuacionPrimerGrado;
+
+namespace ProvesUnitaries
+{
+    [TestClass]
+    public class EcuacionPrimerGradoTest
+    {
+        const float margeError = 0.001f;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            testejarEquacio("2x - 1 = 0", 0.5);
+        }
+
+        void testejarEquacio(string equacio, double resultatEsperat)
+        {
+            EcuacionPrimerGrado.EcuacionPrimerGrado equacioProjecte = new EcuacionPrimerGrado.EcuacionPrimerGrado();
+
+            double resultat = equacioProjecte.obtenerResultado("2x - 1 = 0");
+            Assert.AreEqual(resultatEsperat, resultat, margeError, "Prova fallida");
+        }
+    }
+}
