29e921845018e12ab57db312f47e6c941a929042
icrescenti_linux <46972850+icrescenti@users.noreply.github.com>
2021-04-09 17:51:46 +0200

    EQUACIONS

--- dam_wpf/UF2/EcuacionPrimerGrado/ProvesUnitaries/EcuacionPrimerGradoTest.cs
+++ dam_wpf/UF2/EcuacionPrimerGrado/ProvesUnitaries/EcuacionPrimerGradoTest.cs
@@ -10,16 +10,42 @@ namespace ProvesUnitaries
         const float margeError = 0.001f;
 
         [TestMethod]
-        public void TestMethod1()
+        public void testejarEquacions()
         {
-            testejarEquacio("2x - 1 = 0", 0.5);
+            testejarEquacio("2x - 1 = 0", 0.5f);
+            testejarEquacio("2x + 1 = 0", -0.5);
+            testejarEquacio("2x + 1 = 10", 4.5f);
+            testejarEquacio("2x + 1 = -10", -5.5f);
+            
+            //ERROR
+            //testejarEquacio("hola", -4.5);
         }
 
         void testejarEquacio(string equacio, double resultatEsperat)
         {
             EcuacionPrimerGrado.EcuacionPrimerGrado equacioProjecte = new EcuacionPrimerGrado.EcuacionPrimerGrado();
 
-            double resultat = equacioProjecte.obtenerResultado("2x - 1 = 0");
+            double resultat = equacioProjecte.obtenerResultado(equacio);
+            Assert.AreEqual(resultatEsperat, resultat, margeError, "Prova fallida");
+        }
+    }
+
+    [TestClass]
+    public class ParsejadorTest
+    {
+        const float margeError = 0.001f;
+
+        [TestMethod]
+        public void testejarParts()
+        {
+            testejarParsejador("2x - 1 = 0", 2);
+        }
+
+        void testejarParsejador(string equacio, double resultatEsperat)
+        {
+            EcuacionPrimerGrado.Parseador classeParsejador = new EcuacionPrimerGrado.Parseador();
+
+            double resultat = classeParsejador.obtenerParte1(equacio);
             Assert.AreEqual(resultatEsperat, resultat, margeError, "Prova fallida");
         }
     }
