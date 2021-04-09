82f9dba200d6792152fbd77f904603716f07c002
icrescenti_linux <46972850+icrescenti@users.noreply.github.com>
2021-04-09 15:51:42 +0200

    Project added

---
+++ dam_wpf/UF2/EcuacionPrimerGrado/EcuacionPrimerGrado/EcuacionPrimerGrado.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * Ecuacion en formato:
+ * 
+ * ax + b = c
+ *
+ * a : parte1
+ * b : parte2
+ * + : operador
+ * c: parte3
+ *
+ * Solucion:
+ * x = (c - b) / a
+ * es decir:
+ * 
+ * x = (parte3 - parte2)/parte1
+ */
+
+namespace EcuacionPrimerGrado
+{
+
+    public class EcuacionPrimerGrado
+    {
+		private Parseador parseador = new Parseador();
+
+		public double obtenerResultado(String ecuacion)
+		{
+
+			int parte1 = parseador.obtenerParte1(ecuacion);
+			int parte2 = parseador.obtenerParte2(ecuacion);
+
+			int parte3 = parseador.obtenerParte3(ecuacion);
+
+			double resultado = (double)((parte3 - parte2)) / (double)(parte1);
+
+			return resultado;
+		}
+
+	}
+
+}
---
+++ dam_wpf/UF2/EcuacionPrimerGrado/EcuacionPrimerGrado/Parseador.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcuacionPrimerGrado
+{
+	public class Parseador
+	{
+		public int obtenerParte1( String ecuacion)
+		{
+
+			String[] partes1 = obtenerPartes12(ecuacion);
+
+			String parte1 = partes1[0].Trim();
+
+			return Int32.Parse(parte1.Substring(0, parte1.Length - 1));
+		}
+
+		public int obtenerParte2( String ecuacion)
+		{
+
+			String[] partes1 = obtenerPartes12(ecuacion);
+
+			String parte2 = partes1[1].Trim();
+
+			String operador = obtenerOperador(ecuacion);
+
+			if ("-".Equals(operador))
+			{
+				return Int32.Parse(parte2) * (-1);
+			}
+
+			return Int32.Parse(parte2);
+		}
+
+		public String obtenerOperador( String ecuacion)
+		{
+			if (ecuacion.IndexOf('+') > 0)
+			{
+				return "+";
+			}
+			else
+			{
+				return "-";
+			}
+		}
+
+		public int obtenerParte3( String ecuacion)
+		{
+			String[] partesEcuacion = ecuacion.Split('=');
+			return Int32.Parse(partesEcuacion[1].Trim());
+		}
+
+		private String[] obtenerPartes12( String ecuacion)
+		{
+			String[] partesEcuacion = ecuacion.Split('=');
+
+			String operador = obtenerOperador(ecuacion);
+
+			String[] partes1 = partesEcuacion[0].Split(char.Parse( operador));
+
+			return partes1;
+		}
+	}
+}
