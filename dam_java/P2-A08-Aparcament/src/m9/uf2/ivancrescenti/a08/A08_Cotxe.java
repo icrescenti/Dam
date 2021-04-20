4400bc621c27c0d6b081e0f35c1d104200f8626f
icrescenti_linux <46972850+icrescenti@users.noreply.github.com>
2021-04-20 19:56:59 +0200

    coxe

---
+++ dam_java/P2-A08-Aparcament/src/m9/uf2/ivancrescenti/a08/A08_Cotxe.java
@@ -0,0 +1,25 @@
+package m9.uf2.ivancrescenti.a08;
+
+import java.util.Random;
+
+public class A08_Cotxe extends Thread {
+    String matricula = "0001 AAA";
+    A08_Parquing parking;
+
+    public A08_Cotxe(String nom, A08_Parquing parking, String matricula)
+    {
+        super(nom);
+        this.matricula = matricula;
+        this.parking = parking;
+    }
+
+	public void run() {
+        Random rand = new Random();
+        //Thread.sleep(rand.nextInt(10));
+	}
+
+    String retornarMatricula()
+    {
+        return matricula;
+    }
+}
