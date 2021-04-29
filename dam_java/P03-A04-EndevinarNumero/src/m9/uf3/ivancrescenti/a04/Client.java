5e7c5c4049d1615ed4671bee8ce3148cc240e53a
icrescenti_linux <46972850+icrescenti@users.noreply.github.com>
2021-04-29 19:30:04 +0200

    endevinar numeros

--- dam_java/P03-A04-EndevinarNumero/src/m9/uf3/ivancrescenti/a04/Client.java
+++ dam_java/P03-A04-EndevinarNumero/src/m9/uf3/ivancrescenti/a04/Client.java
@@ -2,16 +2,45 @@ package m9.uf3.ivancrescenti.a04;
 
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 class Client {
 	static final String HOST = "localhost";
-	static final int PORT = 5000;
+	static final int PORT = 2234;
+
 	public static void main(String[] arg) {
+		Scanner escaner = new Scanner(System.in);
+
 		try {
+			//Socket amb la IP i Port a connectarse
 			Socket socketClient = new Socket(HOST, PORT);
-			DataInputStream in = new
-			DataInputStream(socketClient.getInputStream());
-			System.out.println(in.readUTF());
+			
+			//Pipes per rebre i enviar
+			DataOutputStream outStream = new DataOutputStream(socketClient.getOutputStream());
+			DataInputStream in = new DataInputStream(socketClient.getInputStream());
+
+			boolean victoria = false;
+
+			for (int i = 0; i<5 && !victoria; i++) {
+				System.out.println("Numero a endevinar");
+				String msg = escaner.nextLine();
+
+				//enivament missatge
+				outStream.writeUTF(msg);
+				//resposta
+				System.out.println(in.readUTF());
+				//retorna si el numero ha estat acertat
+				victoria = in.readBoolean();
+			}
+
+			if (!victoria)
+			{
+				System.out.println(in.readUTF());
+			}
+			
+			//tenquem pipes
+			escaner.close();
+			outStream.close();
 			in.close();
 			socketClient.close();
 		}
--- dam_java/P03-A04-EndevinarNumero/src/m9/uf3/ivancrescenti/a04/Servidor.java
+++ dam_java/P03-A04-EndevinarNumero/src/m9/uf3/ivancrescenti/a04/Servidor.java
@@ -2,23 +2,59 @@ package m9.uf3.ivancrescenti.a04;
 
 import java.io.*;
 import java.net.*;
+import java.util.Random;
 
 class Servidor {
-	static final int PORT = 5000;
+	static final int PORT = 2234;
 	public static void main(String[] arg) {
 		try {
+			//Iniciem un socket amb el port 2234
 			ServerSocket socketServidor = new ServerSocket(PORT);
 			System.out.println("Escolto el port " + PORT);
-			for (int nClients = 0; nClients < 3; nClients++) {
-				Socket socketServei = socketServidor.accept();
-				System.out.println("Serveixo al client " + nClients);
-				DataOutputStream out = new
-				DataOutputStream( socketServei .getOutputStream());
-				out.writeUTF("Hola client " + nClients);
-				out.close();
-				socketServei .close();
+
+			//Auto-acceptem qualsevol peticio de algun client
+			Socket socketServei = socketServidor.accept();
+
+			//Generem el numero aleatori
+			Random rand = new Random();
+			int numeroAleatori = rand.nextInt(100);
+			System.out.println("(Secret) El numero generat es: " + numeroAleatori);
+
+			//Obrim 2 pipes, per rebre i enviar informacio
+			DataInputStream in = new DataInputStream(socketServei.getInputStream());
+			DataOutputStream outStream = new DataOutputStream(socketServei.getOutputStream());
+			boolean correcte = false;
+
+			try {
+				for (int i = 0; i<5 && !correcte; i++)
+				{
+					//Parseja el valor del client
+					int numero = Integer.parseInt(in.readUTF());
+
+					if (numero == numeroAleatori)
+					{
+						outStream.writeUTF("Molt bé has endevinat el número");
+						correcte = true;
+					}
+					else if (numero < numeroAleatori)
+						outStream.writeUTF("El número a endevinar és major al número " + numero);
+					else
+						outStream.writeUTF("El número a endevinar és menor al número " + numero);
+				
+					outStream.writeBoolean(correcte);
+				}
+
+				if (!correcte)
+					outStream.writeUTF("Has esgotat els intents, el número era el " + numeroAleatori);
+
+			} catch (NumberFormatException e) {
+				outStream.writeUTF("El missatge rebut no és correcte, ha de ser un número entre 1 i 99");
 			}
-			System.out.println("Massa clients per avui");
+
+			//Tenquem les pipes i els sockets
+			outStream.close();
+			in.close();
+			socketServei.close();
 			socketServidor.close();
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
