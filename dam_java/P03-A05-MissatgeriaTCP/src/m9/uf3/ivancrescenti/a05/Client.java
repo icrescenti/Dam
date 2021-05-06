9dbfbbcf87babff2f1347b7df64fa36aa4750762
icrescenti <icrescenti@terkstudios.com>
2021-05-06 20:41:02 +0200

    comments

--- dam_java/P03-A05-MissatgeriaTCP/src/m9/uf3/ivancrescenti/a05/Client.java
+++ dam_java/P03-A05-MissatgeriaTCP/src/m9/uf3/ivancrescenti/a05/Client.java
@@ -7,17 +7,21 @@ import java.util.Scanner;
 
 class Client {
 	public static void main(String[] arg) {
+		//Escaner i Socol
 		Scanner escaner = new Scanner(System.in);
 		Socket socketClient;
 
+		//Host i port
 		String HOST;
 		int PORT;
 
 		try {
 
+			//IP de la maquina
 			InetAddress direccio = InetAddress.getByName("localhost");
 			print(direccio.getHostAddress().toString());
 		
+			//Definicio de host i port
 			print("Insereix les dades del socket\n");
 			print("IP: ");
 			HOST = escaner.nextLine();
@@ -25,10 +29,13 @@ class Client {
 			PORT = escaner.nextInt();
 			escaner.nextLine();
 			
+			//obrir socol
 			socketClient = new Socket(HOST, PORT);
 
+			//obrir pipe per enviar
 			DataOutputStream outStream = new DataOutputStream(socketClient.getOutputStream());
 
+			//enivar missatges fins dir prou
 			Boolean execucio = true;
 			while (execucio)
 			{
@@ -41,7 +48,7 @@ class Client {
 				outStream.writeUTF(msg);
 			}
 			
-
+			//tancar pipes
 			escaner.close();
 			outStream.close();
 			socketClient.close();
--- dam_java/P03-A05-MissatgeriaTCP/src/m9/uf3/ivancrescenti/a05/Servidor.java
+++ dam_java/P03-A05-MissatgeriaTCP/src/m9/uf3/ivancrescenti/a05/Servidor.java
@@ -8,22 +8,29 @@ class Servidor {
 	public static void main(String[] arg) {
 		try {
 			
+			//Escanner ip de la maquina i port
 			Scanner escaner = new Scanner(System.in);
 			InetAddress direccio = InetAddress.getByName("localhost");
 			int PORT;
 
 			print(direccio.getHostAddress().toString() + "\n");
 
+			//Port
 			print("Port a escoltar: ");
 			PORT = escaner.nextInt();
 
+			//Obrir socol
 			ServerSocket socketServidor = new ServerSocket(PORT);
 			System.out.println("Escolto el port " + PORT);
 			
+			//Acceptar connexions
 			Socket socketServei = socketServidor.accept();
 			print("La IP del client es: " + socketServei.getInetAddress());
+
+			//Pipe per rebre
 			DataInputStream in = new DataInputStream(socketServei.getInputStream());
 
+			//Llegir missatges rebuts fins dir prou
 			Boolean execucio = true;
 			while (execucio)
 			{
@@ -34,6 +41,7 @@ class Servidor {
 				print(msg + "\n");
 			}
 
+			//Tencar pipes
 			in.close();
 			socketServei.close();
 			socketServidor.close();
